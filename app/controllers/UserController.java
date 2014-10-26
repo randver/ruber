4983b90dfde91053db1b36107c506478f785482c
momojoejoe <johannl12@ru.is>
2014-10-26 16:56:21 +0000

    get history

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,27 +1,26 @@
 package controllers;
 
 
+import is.ru.honn.ruber.domain.TripDTO;
 import is.ru.honn.ruber.users.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.gethistory;
 
-
-import static play.libs.Json.toJson;
+import java.util.List;
 
 public class UserController extends Controller
 {
   protected static ApplicationContext ctx = new FileSystemXmlApplicationContext("/conf/UserService.xml");
 
-  public static Result getHistory()
+  public static Result getHistory(int uuid)
   {
       UserService service = (UserService) ctx.getBean("userService");
+      List<TripDTO> triplist;
+      triplist = service.getHistory(1);
 
-
-      Object result = new Object();
-      result = service.getHistory(1);
-
-      return ok(toJson(result));
+      return ok(gethistory.render(triplist));
   }
 }
